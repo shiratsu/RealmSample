8f5f1435c3a8067924b9e8cbfc0a0cf0b6348bcd
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-20 15:33:47 +0900

    Initial Commit

---
+++ RealmSample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSample2
+//
+//  Created by HIRATSUKA SHUNSUKE on 2015/02/20.
+//  Copyright (c) 2015年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
